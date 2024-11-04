3368df7542ea6a1bb2502ee65d1786f4335e98b7
Relangimanjusri <manjurelangi2@gmail.com>
2024-11-04 11:01:50 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,8 +1,6 @@
-import java.utill.*;
-import java.lang.*;
 class sample
 {
-public static void   main(string[]args)
+public static void main(String[] args)
 {
 System.out.println("hello world!!");
 }
